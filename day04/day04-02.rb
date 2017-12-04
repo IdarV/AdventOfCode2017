89baaf24a48a1955bd24b644fb328e3ee2757712
idarv <twintip_idar@hotmail.com>
2017-12-04 17:05:46 +0100

    day 4 - 2

--- day04/day04-02.rb
+++ day04/day04-02.rb
@@ -1,1 +1,1 @@
-p File.readlines('input.txt').select{|k| b=k.split(" "); b.map{|k| k.split('').sort}.count == b.map{|k| k.split('').sort}.uniq.count }.count
+p File.readlines('input.txt').select{|k| b=k.split(' '); b.count == b.map{|k| k.chars.sort}.uniq.count}.count
