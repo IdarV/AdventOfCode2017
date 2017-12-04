1f3e11290b500e0de2eeae138860de3b3d4e798c
Idar Vassdal <idarv@mesan.no>
2017-12-04 18:22:14 +0100

    day 4 - 2

--- day04/day04-02.rb
+++ day04/day04-02.rb
@@ -1,1 +1,1 @@
-p File.readlines('input.txt').select{|k| b=k.split(' '); b.count == b.map{|k| k.chars.sort}.uniq.count}.count
+p File.readlines('input.txt').select{|k| b=k.split(' '); b.count == b.map{|l| l.chars.sort}.uniq.count}.count
