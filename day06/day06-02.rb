73c32eef032b8228ac39ce2e09a796a5c80944bb
idarv <twintip_idar@hotmail.com>
2017-12-06 17:17:34 +0100

    day 6 - 2

---
+++ day06/day06-02.rb
@@ -0,0 +1,25 @@
+data = File.read('input.txt').split('	').map(&:to_i)
+history = []
+found = false
+
+def distribute (data, index, value)
+  value.times do
+    index = index + 1
+    index = index - data.length if index >= data.length
+    data[index] = data[index] + 1
+  end
+
+  data
+end
+
+until found
+  highest_index = data.index(data.max)
+  value = data[highest_index]
+  data[highest_index] = 0
+
+  data = distribute(data, highest_index, value)
+  found = true if history.include? data.to_s
+  history << data.to_s
+end
+
+puts history.rindex(data.to_s) - history.index(data.to_s)
